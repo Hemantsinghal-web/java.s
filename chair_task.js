c326b47413a5f754a52bcb32d459a0d291895610
Hemantsinghal-web <singhal2005hemant@gmail.com>
2025-01-11 19:48:22 +0530

    11

---
+++ chair_task.js
@@ -0,0 +1,31 @@
+let head = document.querySelector('#head')
+
+function select()
+{
+    head.style.backgroundColor = "white"
+    
+}
+
+let head2 = document.querySelector('#head2')
+
+function select1()
+{
+    head2.style.backgroundColor = "white"
+      
+}
+
+let head3 = document.querySelector('#head3')
+
+function select2()
+{
+    head3.style.backgroundColor = "white"
+    
+}
+
+let head4 = document.querySelector('#head4')
+
+function select3()
+{
+    head4.style.backgroundColor = "white"
+    
+}
